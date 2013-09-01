2de87f6b426e0363a1b8eeb9ffe094996ddfd50d
Joao Felipe <joaofelipenp@gmail.com>
2013-09-01 08:21:31 -0300

    request messages

--- Android/src/br/uff/hacktonapp/SolicitationScreen.java
+++ Android/src/br/uff/hacktonapp/SolicitationScreen.java
@@ -42,9 +42,9 @@ public class SolicitationScreen extends Activity implements OnItemSelectedListen
 	
 	public void makeStreetRequest(View v){
 		UrbanProblem [] p = { 
-			new UrbanProblem(RequestType.STREET, 0, "rua esburacada"),
-			new UrbanProblem(RequestType.STREET, 1, "rua tensa"),
-			new UrbanProblem(RequestType.STREET, 2, "rua mais tensa"),
+			new UrbanProblem(RequestType.STREET, 2, "Reparo de buraco na pista/rua (7 dias)"),
+			new UrbanProblem(RequestType.STREET, 3, "Reparo de buraco na calçada (15 dias úteis)"),
+			new UrbanProblem(RequestType.STREET, 4, "Reparo de buraco na ciclovia (15 dias úteis)"),
 		};
 		setProblems(p);
 		setFacebookRequestName("conserto de via pública", "um");
@@ -54,9 +54,9 @@ public class SolicitationScreen extends Activity implements OnItemSelectedListen
 	
 	public void makeLightRequest(View v){
 		UrbanProblem [] p = { 
-				new UrbanProblem(RequestType.LIGTH, 0, "rua esburacada"),
-				new UrbanProblem(RequestType.LIGTH, 1, "rua tensa"),
-				new UrbanProblem(RequestType.LIGTH, 2, "rua mais tensa"),
+				new UrbanProblem(RequestType.LIGTH, 5, "Reparo de lâmpada apagada (7 dias)"),
+				new UrbanProblem(RequestType.LIGTH, 6, "Reparo de lâmpada piscando (7 dias)"),
+				new UrbanProblem(RequestType.LIGTH, 7, "Reparo de lâmpada acesa durante o dia (7 dias)"),
 			};
 			setProblems(p);
 		setFacebookRequestName("reparo de iluminação pública", "um");
@@ -66,9 +66,11 @@ public class SolicitationScreen extends Activity implements OnItemSelectedListen
 
 	public void makeParkingRequest(View v){
 		UrbanProblem [] p = { 
-				new UrbanProblem(RequestType.PARK, 0, "rua esburacada"),
-				new UrbanProblem(RequestType.PARK, 1, "rua tensa"),
-				new UrbanProblem(RequestType.PARK, 2, "rua mais tensa"),
+				new UrbanProblem(RequestType.PARK, 8, "Fiscalização de veículo estacionado sobre a calçada (4 horas)"),
+				new UrbanProblem(RequestType.PARK, 9, "Fiscalização de veículo estacionado em vias públicas (4 horas)"),
+				new UrbanProblem(RequestType.PARK, 10, "Fiscalização de veículo estacionado em frente ao portão de garagem (4 horas)"),
+				new UrbanProblem(RequestType.PARK, 11, "Fiscalização de veículo estacionado em local com placa de proibido estacionar (4 horas)"),
+				new UrbanProblem(RequestType.PARK, 12, "Fiscalização de veículo estacionado em ponto de táxi (4 horas)"),
 			};
 			setProblems(p);
 		setFacebookRequestName("solicitação de estacionamento irregular", "uma");
@@ -78,9 +80,8 @@ public class SolicitationScreen extends Activity implements OnItemSelectedListen
 	
 	public void makeTreeRequest(View v){
 		UrbanProblem [] p = { 
-				new UrbanProblem(RequestType.TREE, 0, "rua esburacada"),
-				new UrbanProblem(RequestType.TREE, 1, "rua tensa"),
-				new UrbanProblem(RequestType.TREE, 2, "rua mais tensa"),
+				new UrbanProblem(RequestType.TREE, 0, "Poda de galhos de árvores localizadas em logradouro público (indeterminado)"),
+				new UrbanProblem(RequestType.TREE, 1, "Retirada de árvore (45 dias)")
 			};
 			setProblems(p);
 		setFacebookRequestName("poda de árvore", "uma");
